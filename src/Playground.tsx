ca15ff1941e7b12ff6340b9db47144a5d8eb341e
James Best <james.best@sidekickmoney.com>
2026-04-22 15:08:24 +0100

    feat(playground): wire leva controls to orb props

--- src/Playground.tsx
+++ src/Playground.tsx
@@ -1,6 +1,56 @@
+import { useControls, folder } from "leva";
 import { Orb } from "./orb/Orb";
+import type { OrbColors } from "./orb/types";
 import { DEFAULT_ORB_PROPS } from "./orb/types";
 
 export function Playground() {
-  return <Orb {...DEFAULT_ORB_PROPS} />;
+  const {
+    size,
+    c0,
+    c1,
+    c2,
+    c3,
+    highlightX,
+    highlightY,
+    highlightRadius,
+    blur,
+    grainIntensity,
+    grainScale,
+    rotation,
+  } = useControls({
+    size: { value: DEFAULT_ORB_PROPS.size, min: 100, max: 600, step: 10 },
+    Colors: folder({
+      c0: DEFAULT_ORB_PROPS.colors[0],
+      c1: DEFAULT_ORB_PROPS.colors[1],
+      c2: DEFAULT_ORB_PROPS.colors[2],
+      c3: DEFAULT_ORB_PROPS.colors[3],
+    }),
+    Highlight: folder({
+      highlightX: { value: DEFAULT_ORB_PROPS.highlightX, min: 0, max: 1, step: 0.01 },
+      highlightY: { value: DEFAULT_ORB_PROPS.highlightY, min: 0, max: 1, step: 0.01 },
+      highlightRadius: { value: DEFAULT_ORB_PROPS.highlightRadius, min: 0, max: 1, step: 0.01 },
+    }),
+    Surface: folder({
+      blur: { value: DEFAULT_ORB_PROPS.blur, min: 0, max: 40, step: 0.5 },
+      grainIntensity: { value: DEFAULT_ORB_PROPS.grainIntensity, min: 0, max: 1, step: 0.01 },
+      grainScale: { value: DEFAULT_ORB_PROPS.grainScale, min: 0.5, max: 5, step: 0.1 },
+      rotation: { value: DEFAULT_ORB_PROPS.rotation, min: 0, max: 360, step: 1 },
+    }),
+  });
+
+  const colors: OrbColors = [c0, c1, c2, c3];
+
+  return (
+    <Orb
+      size={size}
+      colors={colors}
+      highlightX={highlightX}
+      highlightY={highlightY}
+      highlightRadius={highlightRadius}
+      blur={blur}
+      grainIntensity={grainIntensity}
+      grainScale={grainScale}
+      rotation={rotation}
+    />
+  );
 }
