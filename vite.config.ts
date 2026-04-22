0adcbbfe61eafc2c28bec72293c7f9a708d2c3d3
James Best <james.best@sidekickmoney.com>
2026-04-22 14:41:04 +0100

    chore: add react-native-skia, canvaskit, leva; copy wasm

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/canvaskit-wasm/bin/full/canvaskit.wasm",
+          dest: ".",
+        },
+      ],
+    }),
+  ],
+  optimizeDeps: {
+    exclude: ["canvaskit-wasm"],
+  },
+});
