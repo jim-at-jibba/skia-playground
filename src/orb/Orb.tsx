9fea305a4a7609bc3813283a231bb312ac21868d
James Best <james.best@sidekickmoney.com>
2026-04-22 14:57:42 +0100

    feat(orb): soften with Blur image filter

--- src/orb/Orb.tsx
+++ src/orb/Orb.tsx
@@ -1,4 +1,5 @@
 import {
+  Blur,
   Canvas,
   Circle,
   Group,
@@ -16,30 +17,32 @@ export function Orb(props: OrbProps) {
     highlightX,
     highlightY,
     highlightRadius,
+    blur,
   } = props;
   const center = vec(size / 2, size / 2);
   const radius = size / 2;
   const wrappedColors = [...colors, colors[0]];
 
   const sweepTransform = [{ rotate: (rotation - 90) * (Math.PI / 180) }];
-
   const highlightCenter = vec(highlightX * size, highlightY * size);
   const highlightR = highlightRadius * size;
 
   return (
     <Canvas style={{ width: size, height: size }}>
-      <Group transform={sweepTransform} origin={center}>
-        <Circle cx={center.x} cy={center.y} r={radius}>
-          <SweepGradient c={center} colors={wrappedColors} />
+      <Group layer={<Blur blur={blur} />}>
+        <Group transform={sweepTransform} origin={center}>
+          <Circle cx={center.x} cy={center.y} r={radius}>
+            <SweepGradient c={center} colors={wrappedColors} />
+          </Circle>
+        </Group>
+        <Circle cx={highlightCenter.x} cy={highlightCenter.y} r={highlightR}>
+          <RadialGradient
+            c={highlightCenter}
+            r={highlightR}
+            colors={["rgba(255,255,255,0.6)", "rgba(255,255,255,0)"]}
+          />
         </Circle>
       </Group>
-      <Circle cx={highlightCenter.x} cy={highlightCenter.y} r={highlightR}>
-        <RadialGradient
-          c={highlightCenter}
-          r={highlightR}
-          colors={["rgba(255,255,255,0.6)", "rgba(255,255,255,0)"]}
-        />
-      </Circle>
     </Canvas>
   );
 }
